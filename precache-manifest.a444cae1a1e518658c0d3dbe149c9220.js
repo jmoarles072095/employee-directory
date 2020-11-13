fad8121faeb4c26c1902659018d9d80eda227d0a
Jose Morales <jmorales072095@gmail.com>
2020-11-13 12:26:07 -0800

    Updates

---
+++ precache-manifest.a444cae1a1e518658c0d3dbe149c9220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58aae01ddc15d99c3891f2bdd15759c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4967145326245b3c57bf",
+    "url": "/static/css/main.12223b22.chunk.css"
+  },
+  {
+    "revision": "0516565484dddfbe8a91",
+    "url": "/static/js/2.acc4bf16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acc4bf16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4967145326245b3c57bf",
+    "url": "/static/js/main.3f408f76.chunk.js"
+  },
+  {
+    "revision": "a19fcb56d3337dbc0165",
+    "url": "/static/js/runtime-main.81ebaa0d.js"
+  }
+]);
