adf1b246fbe53b255d5ba65c5e78300009584161
Jose Morales <jmorales072095@gmail.com>
2020-11-18 10:55:33 -0800

    added files

---
+++ build/precache-manifest.11783f9eab5e97707c570af8dc674e4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc45f487965410865b9c739de55deb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca87e18d0e8e957a3f94",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c59cbff2334d3fa1064d",
+    "url": "/static/js/2.2ba8b590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2ba8b590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca87e18d0e8e957a3f94",
+    "url": "/static/js/main.d8fa3788.chunk.js"
+  },
+  {
+    "revision": "3c07aaa835d62c4efb0f",
+    "url": "/static/js/runtime-main.8a0bb1b0.js"
+  }
+]);
